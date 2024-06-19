8c6257d6e4cd2c1c3c98bd20464449607c1b3cfc
FRA uni <francesca.rossi@upc.edu>
2024-06-19 12:13:21 +0200

    no changes

--- main_original.py
+++ main_original.py
@@ -14,7 +14,7 @@ from stability_analysis.analysis import small_signal
 from stability_analysis.preprocess.utils import *
 from stability_analysis.random_operating_point import random_OP
 from stability_analysis.modify_GridCal_grid import assign_Generators_to_grid,assign_PQ_Loads_to_grid
-from GridCalEngine.Core.DataStructures import numerical_circuit
+from GridCalEngine.DataStructures import numerical_circuit
 
 # %% SET FILE NAMES AND PATHS
 
