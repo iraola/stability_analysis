3cca36dd1d749cd81b60341b017f0ffffca7082a
francesca <francesca.rossi@upc.edu>
2025-07-25 12:41:37 +0200

    test different dolver for PF

--- stability_analysis/powerflow/GridCal_powerflow.py
+++ stability_analysis/powerflow/GridCal_powerflow.py
@@ -21,24 +21,49 @@ def create_model(path_raw, name_raw=None):
 def run_powerflow(grid,solver_type=SolverType.NR, Qconrol_mode=ReactivePowerControlMode.Direct):
             
     # RUN POWERFLOW
-    #for solver_type in [SolverType.IWAMOTO, SolverType.NR, SolverType.LM, SolverType.FASTDECOUPLED]:
+    for solver_type in [SolverType.IWAMOTO, SolverType.NR, SolverType.LM, SolverType.FASTDECOUPLED]:
     
-    print(solver_type)
-
-    options = PowerFlowOptions(solver_type,
-                               verbose=False,
-                               initialize_with_existing_solution=False,
-                               retry_with_other_methods=False,
-                               ignore_single_node_islands = True,
-                               control_q=Qconrol_mode,
-                               tolerance = 1e-10)
-                               #max_iter=2)
+        print(solver_type)
     
-    pf = PowerFlowDriver(grid, options)
-    pf.run()
-    
-    #print('Converged:', pf.convergence_reports[0].converged_[0])
+        options = PowerFlowOptions(solver_type,
+                                   verbose=False,
+                                   initialize_with_existing_solution=False,
+                                   retry_with_other_methods=False,
+                                   ignore_single_node_islands = True,
+                                   control_q=Qconrol_mode,
+                                   tolerance = 1e-6)
+                                   # max_iter=20)
+        
+        pf = PowerFlowDriver(grid, options)
+        pf.run()
+        
+        print('Converged:', pf.convergence_reports[0].converged_[0])
     
     # grid.get_buses()
     return pf
 
+def preprocess_grid(gridCal_grid, d_raw_data):
+
+    df_lines_grid = pd.DataFrame(columns=['bf','bt'])
+    df_trafos_grid = pd.DataFrame(columns=['bf','bt'])
+    
+    
+    if len(gridCal_grid.get_branches()) > len(d_raw_data['branch'])+len(d_raw_data['trafo']):
+        for idx, line in enumerate(gridCal_grid.lines):
+            df_lines_grid.loc[idx,'bf']=line.bus_from.code
+            df_lines_grid.loc[idx,'bt']=line.bus_to.code
+    # Ensure you're comparing with consistent data types
+    df_lines_grid[['bf', 'bt']] = df_lines_grid[['bf', 'bt']].astype(int)
+    df_lines_draw = d_raw_data['branch'][['I', 'J']].astype(int)
+    
+    # Create sets of tuples for fast comparison
+    lines_set_grid = set(map(tuple, df_lines_grid[['bf', 'bt']].values))
+    lines_set_draw = set(map(tuple, df_lines_draw[['I', 'J']].values))
+    
+    # Compute the difference
+    missing_lines = lines_set_grid - lines_set_draw
+    
+    # Convert back to DataFrame for inspection
+    missing_df = pd.DataFrame(list(missing_lines), columns=['bf', 'bt'])
+    
+    print(missing_df)
